dcde616d98e5895f406d41994a9a346ec3df26fc
GustavoBGil <gu.gil@live.com>
2024-08-08 20:51:43 -0300

    Add: UserModel e SessionController

---
+++ backend/src/controllers/SessionController.js
@@ -0,0 +1,31 @@
+/*
+Métodos:
+
+Index: Listagem de sessões
+Store: Criar uma sessão
+Show: Listar uma única sessão
+Update: Atualizar uma sessão
+Destroy: Deletar uma sessão
+
+*/
+
+import User from '../models/User'
+
+class SessionController{
+
+    async store(req , res){
+        const { email } = req.body;
+
+        let user = await User.findOne({ email });
+        if(!user){
+            user = await User.create({ email });
+        }
+
+        
+
+        return res.json(user)
+    }
+
+}
+
+export default new SessionController();
